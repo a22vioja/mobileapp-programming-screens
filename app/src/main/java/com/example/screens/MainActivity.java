3b0817810cd234afc0a0c861f3dca99f1ed6e3c4
a22vioja <a22vioja@student.his.se>
2023-04-20 11:50:46 +0200

    added option to take date from input and show it on showActivity screen

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -19,11 +19,15 @@ public class MainActivity extends AppCompatActivity {
         showButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
+                EditText nameEdit = findViewById(R.id.editTextName);
+                String name = nameEdit.getText().toString();
 
+                EditText shoeSizeEdit = findViewById(R.id.editTextShoeSize);
+                String shoeSize = nameEdit.getText().toString();
 
                 Intent intent = new Intent(MainActivity.this, ShowActivity.class);
-                intent.putExtra("name", "Violeta"); // Optional
-                intent.putExtra("number", 1); // Optional
+                intent.putExtra("name", name); // Optional
+                intent.putExtra("shoeSize", shoeSize ); // Optional
                 startActivity(intent);
             }
         });
